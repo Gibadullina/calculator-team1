cd330e68af466e2a1c82dc3e19cdf87422cb999c
gazindr <yeenreallly@gmail.com>
2021-05-22 15:07:39 +0500

    Добавлен функционал абстрактному классу BaseSolver

--- src/main/java/com/skylabs/baselogic/BaseSolver.java
+++ src/main/java/com/skylabs/baselogic/BaseSolver.java
@@ -1,5 +1,14 @@
 package com.skylabs.baselogic;
 
 public abstract class BaseSolver {
-	int ID;
+	static final double mrot = 12130d;
+	static double perc,c,nt,pt,lt,e,k;
+	static boolean useMrot;
+	static double lastResult = 0;
+
+	//возвращаем последнее расчитанное значение
+	protected static double GetLastRes()
+	{
+		return lastResult;
+	}
 }
