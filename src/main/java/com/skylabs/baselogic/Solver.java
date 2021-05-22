8105ac3789c1aa903b77cdffc2c8acbcbbe7aa65
gazindr <yeenreallly@gmail.com>
2021-05-22 15:10:01 +0500

    Изменен класс Solver.java.

--- src/main/java/com/skylabs/baselogic/Solver.java
+++ src/main/java/com/skylabs/baselogic/Solver.java
@@ -1,24 +1,33 @@
 package com.skylabs.baselogic;
 
-public class Solver {
-	static final double mrot = 12130d;
+public class Solver extends BaseSolver{
+
+//Расчёт по формуле
+public static double Solve(double _perc, double _c, double _nt, double _pt, double _lt, double _e, double _k, boolean _useMrot) {
+	perc = _perc; // налог
+	c = _c; // количество деталей
+	nt = _nt; // количество деталей на получение полной премии
+	pt = _pt; // премия
+	lt = _lt; // норма
+	e = _e; // цена детали
+	k = _k; // районный коэф
+	useMrot = _useMrot; //используется ли мрот
 	
-	//Расчёт по формуле
-	public static double Solve(double perc, double c, double nt, double pt, double lt, double e, double k, boolean useMrot) {
-		double m, result;
-		result = 0;
-		
-		if (nt > 0 && lt < c) { //если параметры премии заданы и было произведено деталей свыше нормы
-			m = e*c + (c-lt)/nt*pt;
-		}
-		else { // иначе считаем без учета премии
-			m = e*c;
-		}
-		if (useMrot && m < mrot) { //Учет МРОТ
-			result = mrot*k*(1-perc/100);
-		} else {
-			result = m*k*(1-perc/100);
-		}
-		return result;
+	double result = 0;
+	double m = 0;
+	if (nt > 0 && lt < c) { //если параметры премии заданы и было произведено деталей свыше нормы
+		m = e*c + (c-lt)/nt*pt;
+	}
+	else { // иначе считаем без учета премии
+		m = e*c;
+	}
+	if (useMrot && m < mrot) { //Учет МРОТ
+		result = mrot*k*(1-perc/100);
+	} else {
+		result = m*k*(1-perc/100);
+	}
+	
+	lastResult = result;
+	return result;
 	}
 }
